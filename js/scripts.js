d9df2b05d8037eb6768c7e57355663782f3a4a80
Austin Landis <austinlandiscode@gmail.com>
2021-01-22 09:40:21 -0800

    Add .submit function with event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
+  $("#form-one").submit(function(event) {
+    event.preventDefault();
+
+
 
   for (let [i] = 0; [i] <= userNumber; [i]++) {
-    
+    if ([i] )
   }
+})
 })
