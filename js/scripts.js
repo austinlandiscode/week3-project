40b2c2c513d985968eb1d54c68a13fb8fd2a2f48
Austin Landis <austinlandiscode@gmail.com>
2021-01-22 11:35:54 -0800

    grasp at straws.

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,11 @@ console.log(parseInt(userNumber));
 let emptyArray = [];
 
   for (let index = 0; index <= parseInt(userNumber); index++ ) {
-    if (userArray.includes("3")) {
+    if (index === 3) {
       emptyArray.push("Won't you be my neighbor?");
-  } else if (userArray.includes("2")) {
+  } else if (index === 2) {
       emptyArray.push("Boop");
-  } else if (userArray.includes("1")) {
+  } else if (index === 1) {
       emptyArray.push("Beep");
   } else {
       emptyArray.push(index);
