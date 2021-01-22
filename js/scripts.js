e15a2515a97a0e4f275163e5b962c1c04d3d01aa
Austin Landis <austinlandiscode@gmail.com>
2021-01-22 11:04:31 -0800

    Add a broken branching.

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,19 @@ let result = userArray.map(index=>Number(index));
 
 console.log(result);
 console.log(parseInt(userNumber));
+let emptyArray = [];
 
-  for (let [i] = 0; [i] <= parseInt(userNumber); [i]++) {
-    if ([i] )
+  for (let index = 0; index <= parseInt(userNumber); index++ ) {
+    if (userArray.includes("3")) {
+      emptyArray.push("Won't you be my neighbor?");
+  } else if (userArray.includes("2")) {
+      emptyArray.push("Boop");
+  } else if (userArray.includes("1")) {
+      emptyArray.push("Beep");
+  } else {
+      emptyArray.push(index);
   }
-});
+  console.log(emptyArray);
+}
+  });
 });
