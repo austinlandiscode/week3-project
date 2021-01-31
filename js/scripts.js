250ab65ca4f827cef4844743e43b988a4af950a7
Austin Landis <austinlandiscode@gmail.com>
2021-01-31 12:07:21 -0800

    fix the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,33 @@
+function mrRob(num) {
+  let countArray = [];
+
+  for (let i = 0; i <= num; i++ ) {
+    i += '';
+    if (i.indexOf('3') > -1) {
+      countArray.push("Won't you be my neighbor?");
+    } else if (i.indexOf('2') > -1) {
+        countArray.push("Boop!");
+    } else if (i.indexOf('1') > -1) {
+        countArray.push("Beep!");
+    } else {
+        countArray.push(i);
+    }
+  }
+  return countArray;
+}
+
 $(document).ready(function() {
   $("form#form-one").submit(function(event) {
     event.preventDefault();
     $("#answer").empty();
-
-    let userNumber = $("input#user-number").val();
-
-    $("#counts").text(userNumber);
+    let userNumber = parseInt($("input#user-number").val());
+    
     $(".card").show();
     $("h4").show();
 
-    let countArray = [];
-
-    for (let i = 0; i <= parseInt(userNumber); i++ ) {
-      i += '';
-      if (i.indexOf('3') > -1) {
-       countArray.push("Won't you be my neighbor?");
-      } else if (i.indexOf('2') > -1) {
-         countArray.push("Boop!");
-      } else if (i.indexOf('1') > -1) {
-         countArray.push("Beep!");
-      } else {
-         countArray.push(i);
-      }
-    }
+    let roboArray = mrRob(userNumber)
     
-   countArray.forEach(function(element) {
+    roboArray.forEach(function(element) {
       $("#answer").append(`<li>${element}</li>`);
     });
   });
